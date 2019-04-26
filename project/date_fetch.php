29cdc648833dc6a223fa9da89b3fc20a80f934c5
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-25 20:50:26 -0700

    Final code for dashboard

---
+++ project/date_fetch.php
@@ -0,0 +1,34 @@
+<?php
+header('Content-Type: application/json');
+
+require_once('connect.php');
+
+     $from = $_GET["from"];
+	 $to = $_GET["to"];
+	
+	
+	$sql = "SELECT * FROM final WHERE date BETWEEN '$from' AND '$to'";
+//	"SELECT * FROM final WHERE Lights BETWEEN '" . $from . "' AND  '" . $to . "'";
+	
+	//
+	
+	$result= mysqli_query($connection, $sql);
+    //	or die (mysql_error ());
+	
+	
+	$data = array();
+	
+	foreach ($result as $row) {
+	$data[] = $row;
+	}
+	
+	
+
+	//On page 1
+	session_start();
+	$_SESSION['varname'] = $data;
+	//printing these values is needed to generate the graph
+	//print json_encode($data);
+	header("Location: date_fetch.html")
+	
+?>
