51a51af881a3bacd1d33ee2a689fdafb9ed8ad33
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-25 20:50:52 -0700

    Final code for dashboard

---
+++ project/temp.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json');
+
+require_once('connect.php');
+		
+	$sql = "SELECT * FROM final";
+	
+	//
+	
+	$result= mysqli_query($connection, $sql);
+    //	or die (mysql_error ());
+	
+	
+	$data = array();
+	
+	foreach ($result as $row) {
+	$data[] = $row;
+	}
+	
+	
+
+	//On page 1
+	//session_start();
+	//$_SESSION['varname'] = $data;
+	//printing these values is needed to generate the graph
+	print json_encode($data);
+	//header("Location: temp.html")
+	
+?>
