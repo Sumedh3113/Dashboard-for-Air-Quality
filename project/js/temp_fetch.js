c273ac7bd99e2b85361e14257151a2da1d64500c
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-25 20:53:06 -0700

    Final code for dashboard

---
+++ project/js/temp_fetch.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $.ajax({
+    url : "http://iotdashboard.atwebpages.com/temp.php",
+    type : "GET",
+    success : function(data){
+      console.log(data);
+
+      var userid = [];
+      var sensor_data = [];
+	 // var facebook_follower2 = [];
+     
+
+      for(var i in data) {
+        userid.push("Date " + data[i].date);
+        sensor_data.push(data[i].Temperature);
+          }
+
+      var chartdata = {
+        labels: userid,
+        datasets: [
+          {
+            label: "sensor1",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(59, 89, 152, 0.75)",
+            borderColor: "rgba(59, 89, 152, 1)",
+            pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
+            pointHoverBorderColor: "rgba(59, 89, 152, 1)",
+            data: sensor_data
+          }
+          
+        ]
+      };
+
+      var ctx = $("#mycanvas");
+
+      var LineGraph = new Chart(ctx, {
+        type: 'line',
+        data: chartdata
+      });
+    },
+    error : function(data) {
+
+    }
+  });
+});
